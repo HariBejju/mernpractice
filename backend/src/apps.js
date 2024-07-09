85d98ed2d1d0926d2859dc585c08af4978bd7616
Hariprasanna <85726444+HariBejju@users.noreply.github.com>
2024-07-09 19:32:07 +0530

    removed cors

--- backend/src/apps.js
+++ backend/src/apps.js
@@ -7,12 +7,22 @@ import cors from "cors"
 const app = express()
 dotenv.config()
 //middlewares
-app.use(cors({
-    origin: "https://merngpt-front-end.vercel.app",
-    methods: ["POST", "GET", "PUT", "DELETE"], // include all methods you plan to use
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://merngpt-front-end.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+// OPTIONS handler for preflight requests
+app.options("*", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://merngpt-front-end.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(204);
+});
 app.use(express.json())
 
 //remove it in production
